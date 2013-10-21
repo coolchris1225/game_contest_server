1c21cf953cd04c9cd413395edf39b0cb929d683c
coolchris1225 <chris_hanson2@taylor.edu>
2013-10-21 13:04:27 +0000

    Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :ensure_user_logged_in, only: [:edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]  
+  before_action :ensure_admin_user, only: [:destroy]
   def index
     @users = User.all
   end
@@ -24,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
   end
     
-  def update
-    @user = User.find(params[:id])
+  def update  
     if @user.update_attributes(permittedparams)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -48,4 +48,17 @@ class UsersController < ApplicationController
     def permittedparams
       permittedparams = params.require(:user).permit(:username,:password,:password_confirmation,:email)
     end    
+    
+    def ensure_user_logged_in
+      redirect_to login_path unless logged_in?
+    end
+    
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)      
+    end
+    
+    def ensure_admin_user
+      redirect_to users_path unless current_user.admin?
+    end
 end
